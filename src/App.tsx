ab76de49f587492871109858471a8429f61ca7be
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-12-31 14:45:55 +0100

    Add styles

--- src/App.tsx
+++ src/App.tsx
@@ -16,19 +16,21 @@ const App = () => {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} alt="Vite logo" />
+          <img src={viteLogo} className="h-24 p-6" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} alt="React logo" />
+          <img src={reactLogo} className="h-24 p-6 animate-spin-slow" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <button onClick={handleClick}>one up</button>
-      <p>{bears}</p>
-      <div>
+      <h1 className="my-12">Vite + React</h1>
+      <div className="mb-12">
+        <button onClick={handleClick} className="bg-gray-50 px-5 py-2 rounded-md mb-2">Increase</button>
+        <p>{bears}</p>
+      </div>
+      <div className="mb-12">
         <p>Edit <code>src/App.tsx</code> and save to test HMR</p>
       </div>
-      <p>Click on the Vite and React logos to learn more</p>
+      <p className="text-gray-400 text-sm">Click on the Vite and React logos to learn more</p>
     </>
   )
 }
